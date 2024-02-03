aaa6365bfb5d907bd9800ea125b51ea86bdc0bb2
ndtd128 <95179216+ndtd128@users.noreply.github.com>
2024-02-03 22:24:31 +0700

    refactor: change icon and add sample folder

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
         setUp();
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/ChooseToolScene.fxml")));        Scene scene = new Scene(root);
         stage.setResizable(false);
-        stage.setTitle("AUT tool");
+        stage.setTitle("NTD-AUT tool");
         stage.setScene(scene);
         stage.show();
     }
